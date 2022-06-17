3f8609ef73df7274af1d197440b7e2c91d2e36b6
Priyanshu <priyanshumaurya007@gmail.com>
2022-06-17 17:07:22 +0530

    updating

--- keyboard/app.js
+++ keyboard/app.js
@@ -1,11 +1,38 @@
 let dataKeyValue, randomValue;
 
+
+// random value generator to jiggle a random key
+
 const randomValueGenerator = ( ) => {
 
     randomValue = Math.floor((Math.random() * 53) + 1);
-    document.getElementById("" + randomValue).classList.add("jiggle");
+    document.getElementById('' + randomValue).classList.add("jiggle");
+    dataKeyValue = document.getElementById('' + randomValue).getAttribute("data-key");
+    console.log("new key jiggled");
 
 };
 
+// function to remove jiggle 
 
+const removeJiggle = () =>{
+    document.getElementById("" + randomValue).classList.remove("jiggle");;
+    console.log("jiggle removed");
+}
+
+// when any key get pressed
+
+
+document.addEventListener('keydown', function(event){
+
+    let pressedKey = event.key.toUpperCase();
+
+    if(dataKeyValue == pressedKey)
+    {
+        removeJiggle();
+        randomValueGenerator();
+    }
+
+})
+
+// first call 
 randomValueGenerator();
