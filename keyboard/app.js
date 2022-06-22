784fa209af53aa559ff9db02c50b1053397c1717
Priyanshu <priyanshumaurya007@gmail.com>
2022-06-22 16:09:39 +0530

    updating

---
+++ keyboard/app.js
@@ -0,0 +1,45 @@
+let dataKeyValue, randomValue;
+let keyAll = document.getElementsByClassName('key');
+
+// random value generator to jiggle a random key
+
+const randomValueGenerator = () => {
+  randomValue = Math.floor(Math.random() * 53);
+  keyAll[randomValue].classList.add('jiggle');
+  dataKeyValue = keyAll[randomValue].getAttribute('data-key');
+  console.log('new key jiggled');
+};
+
+// function to remove jiggle
+
+const removeJiggle = () => {
+  keyAll[randomValue].classList.remove('jiggle');
+  console.log('jiggle removed');
+};
+
+// when any key get pressed
+
+document.addEventListener('keydown', function (event) {
+  console.log(event);
+  let pressedKey = event.key.toUpperCase();
+  if (pressedKey == 'TAB') {
+    event.preventDefault();
+  }
+  if (dataKeyValue == pressedKey) {
+    removeJiggle();
+    randomValueGenerator();
+  } else {
+    document
+      .querySelector(`button[data-key="${pressedKey}"]`)
+      .classList.add('errorKey');
+
+    setTimeout(function () {
+      document
+        .querySelector(`button[data-key="${pressedKey}"]`)
+        .classList.remove('errorKey');
+    }, 200);
+  }
+});
+
+// first call
+randomValueGenerator();
---
+++ piano/app.js
@@ -0,0 +1,14 @@
+// store all classes
+const allKey = document.querySelectorAll('.key');
+
+// function to play song using id
+const playSong = (element, idx) => {
+  let audio = new Audio('audio/key-' + (idx + 1) + '.mp3');
+  console.log(idx);
+  audio.play();
+};
+
+// use to call playSong function
+allKey.forEach((element, idx) => {
+  element.addEventListener('click', () => playSong(element, idx));
+});
